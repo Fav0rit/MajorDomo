6815230056058d367636ff407d99d0bc0eb898d9
Fav0rit <pfavorit@gmail.com>
2018-12-06 17:58:00 +0300

    Add files via upload

--- lib/favorit.class.php
+++ lib/favorit.class.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 function dirsize( $d )
 { 
@@ -49,7 +49,7 @@ if (is_dir($dir))
 				if ($time>$expire_time)	unlink($filepath); // удаление старых файлов
              	if ($max_file_size!='') // если задан $max_file_size
                 {
-             		if (filesize($filepath)>$max_file_size*1024*1024) unlink($filepath);
+             		if (filesize($filepath)>$max_file_size*1024) unlink($filepath);
                 }
 			}
 		}
@@ -81,41 +81,139 @@ safe_exec("mysqlcheck -o db_terminal phistory -u ".DB_USER." -p".DB_PASSWORD);
 }
 
 
-function backup_majordomo($type)
+function backup_majordomo($type,$ftp_backup)
 {
-chdir(ROOT);
-include_once("./config.php");
-include_once("./lib/loader.php");
-include_once("./lib/threads.php");
+	chdir(ROOT);
+	include_once("./config.php");
+	include_once("./lib/loader.php");
+	include_once("./lib/threads.php");
 
-// connecting to database
-$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
+	// connecting to database
+	$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
 
-include_once("./load_settings.php");
-include_once(DIR_MODULES . "saverestore/saverestore.class.php");
+	include_once("./load_settings.php");
+	include_once(DIR_MODULES . "saverestore/saverestore.class.php");
 
-$sv=new saverestore();
+	$sv=new saverestore();
 
-global $design;
-global $code;
-global $data;
-global $save_files;
-$data=1;
-$design=1;
-$code=1;
-$save_files=1;	
-if ($type=="db") {
-$design=0;
-$code=0;
-$save_files=0;
+	global $design;
+	global $code;
+	global $data;
+	global $save_files;
+	$data=1;
+	$design=1;
+	$code=1;
+	$save_files=1;	
+	if ($type=="db") {
+	$design=0;
+	$code=0;
+	$save_files=0;
+	}
+	$res=$sv->dump($out, 1);
+	$sv->removeTree(ROOT.'cms/saverestore/temp');
+	
+	// Кидаем бэкап на ftp-сервер
+	if ($ftp_backup) {
+		$filepath = ROOT.'cms/saverestore/';
+		foreach(glob($filepath . '*.tgz') as $file) {
+		// Получаем последний добавленный/измененный файл
+		$LastModified[] = filemtime($file); // массив файлов со временем изменения файла
+		$FileName[] = $file; // массив всех файлов
+		}
+		 
+		// Сортируем массив с файлами по дате изменения
+		$files = array_multisort($LastModified, SORT_NUMERIC, SORT_ASC, $FileName);
+		$lastIndex = count($LastModified) - 1;
+		// И вот он наш последний добавленный или измененный файл
+		$LastModifiedFile =  $FileName[$lastIndex];
+		
+		echo "Файл для загрузки на ftp: $LastModifiedFile \n";
+		// Загружаем файл на FTP
+		
+		$ftp_server=gg("backup_ftp_server");
+		$ftp_user_name=gg("backup_ftp_login");
+		$ftp_user_pass=gg("backup_ftp_password");
+		$ftp_error=false;
+		if ($ftp_server=='') $ftp_error=true;
+		if ($ftp_user_name=='') $ftp_error=true;
+		if ($ftp_user_pass=='') $ftp_error=true;
+		if (!$ftp_error) ftp_send($LastModifiedFile,$ftp_server,$ftp_user_name,$ftp_user_pass);
+		
+	}
+	
 }
-$res=$sv->dump($out, 1);
-$sv->removeTree(ROOT.'cms/saverestore/temp');
-}
 
-function sync_favorit()
+// Отправка файла на FTP сервер в свою папку
+function ftp_send($filename,$ftp_server,$ftp_user_name,$ftp_user_pass)
 {
-	$url = 'https://github.com/Fav0rit/MajorDomo/raw/master/favorit.tar.gz';
+	$remote_folder=gg("homeName");
+	if ($remote_folder=='') $remote_folder=gg("ThisComputer.Serial");
+	
+	//$remote_file = "/".$remote_folder.'/'.basename($filename);
+	$remote_file = basename($filename);
+	$log="";
+	
+	if ($conn_id = ftp_connect($ftp_server))
+	{
+		echo "Соединение установлено\n";
+		$log.="Соединение установлено\n";
+	}
+	else
+	{
+		echo "Подключение к серверу не удалось\n";
+		$log.="Подключение к серверу не удалось\n";
+		say("Ошибка при выгрузке на FTP: \n".$log,2);
+		return;
+	}
+	
+	if ($login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass)) 
+	{
+		echo "Логин успешен \n";
+		$log.="Логин успешен \n";
+	}
+	else 
+	{
+		echo "Неправильный логин или пароль \n";
+		$log.="Неправильный логин или пароль \n";
+		say("Ошибка при выгрузке на FTP: \n".$log,2);
+		return;
+	}
+	// Включаем соединение в пассивном режиме
+	ftp_pasv($conn_id, true);
+	
+	// Меняем директорию для бэкапа
+	if (ftp_chdir($conn_id, $remote_folder)) {
+    echo "Перехожу в директорию: " . ftp_pwd($conn_id) . "\n";
+	$log.="Перехожу в директорию: " . ftp_pwd($conn_id) . "\n";
+	} else { 
+		echo "Не удалось сменить директорию, пробую её создать\n";
+		// попытка создания директории $dir
+		if (ftp_mkdir($conn_id, $remote_folder)) {
+			echo "Создана директория $remote_folder\n";
+			$log.="Создана директория $remote_folder\n";
+		} else {
+			echo "Не удалось создать директорию $remote_folder\n";
+			$log.="Не удалось создать директорию $remote_folder\n";
+			say("Ошибка при выгрузке на FTP: \n".$log,2);
+		}
+	}
+	
+	if (ftp_put($conn_id, $remote_file, $filename, FTP_ASCII)) {
+		echo "$remote_file успешно загружен на сервер\n";
+		$log.="Файл $remote_file успешно загружен на сервер\n";
+	} else {
+		echo "Не удалось загрузить $file на сервер\n";
+		$log.="Не удалось загрузить $file на сервер\n";
+		say("Ошибка при выгрузке на FTP: \n".$log,2);
+	}
+	
+	ftp_close($conn_id);
+}
+
+
+function sync_favorit($branch="master")
+{
+	$url = "https://github.com/Fav0rit/MajorDomo/raw/$branch/favorit.tar.gz";
 
 	if (!is_dir(ROOT . 'cms/saverestore'))
 	{
@@ -144,13 +242,20 @@ function sync_favorit()
 		exec('tar xzvf ./cms/saverestore/favorit.tar.gz --overwrite-dir', $output, $res);
 		
 	say("Синхронизация скриптов завершена",0);
+	favoritInit();
 }
 
 function calcSunsetSunrise() {
 $lat=gg('ThisComputer.latitude');   // широта
 $long=gg('ThisComputer.longitude'); // долгота
-if ($lat=='') $lat=51.72; //Kursk
-if ($long=='') $long=36.16; //Kursk
+if ($lat=='') {
+	$lat=51.72; //Kursk
+	sg('ThisComputer.latitude',$lat);
+}
+if ($long=='') {
+	$long=36.16; //Kursk
+	sg('ThisComputer.longitude',$long);
+}
 
 $sun_info = date_sun_info(time(), $lat, $long);
 
@@ -189,5 +294,28 @@ $sun_info = date_sun_info(time(), $lat, $long);
 		//echo 'Конец вечерних сумерек: '.date("H:i", $val).'<br>';
 		//setGlobal('ThisComputer.civil_end',date("H:i", $val));
 		}
+	}
+}
+
+function favoritInit() {
+	if (gg("calcSunsetSunrise")=='') sg("calcSunsetSunrise",1);
+	if (gg("everydayBackupDB")=='') sg("everydayBackupDB",1);
+	if (gg("everydayClearHistory")=='') sg("everydayClearHistory",0);
+	if (gg("everydaySyncFavorit")=='') sg("everydaySyncFavorit",1);
+	if (gg("autoClearDebmesBackup")=='') sg("autoClearDebmesBackup",1);
+	if (gg("autoCheckFreePace")=='') sg("autoCheckFreePace",1);
+	if (gg("autoCheckDebmesSize")=='') sg("autoCheckDebmesSize",1);
+	
+	// Добавляем ежеминутное выполнение скрипта newMinute.php если его нет
+	$Record = SQLSelectOne("SELECT * FROM objects WHERE TITLE='ClockChime'");
+	$objID = $Record['ID'];
+
+	$Record = SQLSelectOne("SELECT * FROM methods WHERE TITLE='onNewMinute' AND OBJECT_ID=$objID");
+	$code=$Record['CODE'];
+
+	if ((strpos($code,"require(ROOT.'scripts/newMinute.php');"))==false)
+	{
+		 $Record['CODE']=$code."\n\n"."require(ROOT.'scripts/newMinute.php');";
+		 SQLUpdate('methods', $Record);
 	}
 }
--- scripts/newMinute.php
+++ scripts/newMinute.php
@@ -1,79 +1,105 @@
 <?php
 
+$ver="06122018";
+if (gg("favoritVer")!=$ver)
+{
+	favoritInit();
+	sg("favoritVer",$ver);
+}
+
 $h=(int)date('G',time());
 $m=date('i',time());
 
 // Рассчитываем время восхода и захода Солнца
-if (timeIs("01:00")) {
+if (timeIs("01:00")&&(gg("calcSunsetSunrise")))
+{
 	calcSunsetSunrise();
 }
 
 // Делаем бэкап базы данных
-if (timeIs("02:30")) {
-	backup_majordomo("db");
+if (timeIs("02:15")&&(gg("everydayBackupDB")))
+{
+	if (date('w')==0)
+	{
+		$command='backup_majordomo("full",true);'
+	}
+	else
+	{
+		$command='backup_majordomo("db",true);'
+	}
+	setTimeOut("backup",$command,rand(5,30)*60);
 }
 
 // Запускаем синхронизацию файлов
-if (timeIs("03:00")) {
+if (timeIs("03:00")&&(gg("everydaySyncFavorit")))
+{
 	sync_favorit();
 }
 
 // Очищаем историю
-if (timeIs("03:30")) {
-	clear_history();
+if (timeIs("03:30")&&(gg("everydayClearHistory")))
+{
+	// Пока не актуально в связи с исправлением ошибки
+	//clear_history();
 }
 
-if (timeIs("04:00")) {
-// Удаляем в папке saverestore файлы старше 30 дней
-$folder = ROOT.'cms/saverestore/';
-clear_files($folder,30,'');
+if (timeIs("04:00"))
+{
+	// Удаляем в папке saverestore файлы старше 30 дней
+	$folder = ROOT.'cms/saverestore/';
+	clear_files($folder,30,'');
 
-// Чистим логи
-$folder = ROOT.'cms/debmes/';
-clear_files($folder,7,1);
+	// Чистим логи
+	$folder = ROOT.'cms/debmes/';
+	clear_files($folder,7,200);
 }
 
 
 // Каждые 10 минут контролируем размеры папок
-if (($m%10)==0) {
-$folder=ROOT.'cms/debmes';
-$foldersize=dirsize($folder);
-	if ($foldersize>10*1024)
-	{
-	say("Внимание! Папка ".$folder." занимает ".$foldersize."Kb"." запускаю очистку",2);
-	clear_files($folder,7,1);
-	}
+if ((($m%10)==0))
+{
+	$folder=ROOT.'cms/debmes';
+	$foldersize=dirsize($folder);
+		if ($foldersize>10*1024)
+		{
+			say("Внимание! Папка ".$folder." занимает ".$foldersize."Kb"." запускаю очистку",2);
+			clear_files($folder,7,200);
+		}
 }
 
 // Каждые 15 минут проверяем свободное место на диске
-if (($m%15)==0) {
-$max_usage=90; //%
-$output=array();
-exec('df -h',$output);
-//var_dump($output);
-$fullOutput="";
-$problems=0;
-$problems_details='';
-foreach($output as $line) {
- if (preg_match('/(\d+)% (\/.+)/',$line,$m))
-   $proc=$m[1];
-   $path=$m[2];
-   $fullOutput.=$line."\n";
-   if ($proc>$max_usage) {
-    $problems++;
-    $problems_details.="$path: $proc; ";
-   }
-   //echo "$path: $proc%<br/>";
-}
+if ((($m%15)==0)&&(gg("autoCheckFreeSpace")))
+{
+	$max_usage=90; //%
+	$output=array();
+	exec('df -h',$output);
+	//var_dump($output);
+	$fullOutput="";
+	$problems=0;
+	$problems_details='';
+	foreach($output as $line)
+	{
+		if (preg_match('/(\d+)% (\/.+)/',$line,$m))
+		   $proc=$m[1];
+		   $path=$m[2];
+		   $fullOutput.=$line."\n";
+		   if ($proc>$max_usage)
+		   {
+				$problems++;
+				$problems_details.="$path: $proc; ";
+		   }
+		   //echo "$path: $proc%<br/>";
+	}
 
-// Оповещение
-if (((gg("SpaceProblems"))==0)&&($problems!=0)) {
- $message="На диске заканчивается свободное место! \n";
- $message.=$fullOutput;
- say($message,3);
-}
+	// Оповещение
+	if (((gg("SpaceProblems"))==0)&&($problems!=0))
+	{
+		 $message="На диске заканчивается свободное место! \n";
+		 $message.=$fullOutput;
+		 say($message,3);
+	}
 
-sg("ThisComputer.SpaceDetails",$fullOutput);
-sg("ThisComputer.SpaceProblems",$problems);
-sg("ThisComputer.SpaceProblems_Details",$problems_details);
+	sg("ThisComputer.SpaceDetails",$fullOutput);
+	sg("ThisComputer.SpaceProblems",$problems);
+	sg("ThisComputer.SpaceProblems_Details",$problems_details);
 }
